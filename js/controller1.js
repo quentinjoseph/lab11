4b3f76526aa79ed9e985ccc9c3fb974e28bab2d7
Quentin Joseph <qej2@icloud.com>
2017-05-29 21:54:34 -0400

    commit 1

--- js/controller1.js
+++ js/controller1.js
@@ -2,6 +2,23 @@ var app=angular.module('redMod');
 
 app.controller('controller1', function($scope, services){
 $scope.redditPostsFinal =[];
+var savPost = [];
+var img=angular.element(img);
+$scope.goAway = function(index){
+  console.log('yo');
+  $scope.savPost.splice(index,1);
+
+}
+$scope.saveIT= function(post){
+  if (img.hasClass('saved')){
+    $scope.savPost.pop();
+    console.log(savPost);
+  }else{
+    savPost.push({title: post.title, url: post.url});
+    console.log(savPost);
+  }
+
+}
 $scope.getReddit = function(name){
 // console.log($scope.name);
 $scope.redditPostsFinal.splice(0, 27);
---
+++ js/controller2.js
@@ -0,0 +1,8 @@
+// var app=angular.module('redMod');
+//
+// app.controller('saveStory', function($scope){
+// $scope.saveIT= function(){
+//   console.log('hey');
+// }
+//
+// });
